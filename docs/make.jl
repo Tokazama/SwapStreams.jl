ac07f1cc32eec651d2908f67cbabab021733ecca
Zachary P. Christensen <zchristensen7@gmail.com>
2020-08-22 05:12:50 -0400

    Fix make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -4,7 +4,7 @@ using SwapStreams
 makedocs(
     modules=[SwapStreams],
     format=Documenter.HTML(),
-    repo="https://github.com/JuliaNeuroscience/NeuroCore.jl/blob/{commit}{path}#L{line}",
-    sitename="NeuroCore.jl",
+    repo="https://github.com/Tokazama/SwapStreams.jl/blob/{commit}{path}#L{line}",
+    sitename="SwapStreams.jl",
     authors="Zachary P. Christensen",
 )
